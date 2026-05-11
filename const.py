47c491692325008c0394682e4d3280190a5cacf5
Vinicius Borin Zenbook <borin.vini@gmail.com>
2026-05-11 18:10:54 +0300

    fix: add type hints to TRAIN_CATEGORIES, TRAIN_TYPES, MAP_CENTER, MAP_ZOOM

--- const.py
+++ const.py
@@ -16,8 +16,8 @@ AVAILABLE_MONTHS: list[int] = list(range(1, 13))
 TRAIN_FILE_PATTERN = "all_trains_data_{year}_{month:02d}.csv"
 WEATHER_FILE_PATTERN = "fmi_weather_observations_{year}_{month:02d}.csv"
 
-TRAIN_CATEGORIES = ["Long-distance", "Commuter", "Cargo"]
-TRAIN_TYPES = ["IC", "S", "PYO", "HDM", "HL", "T"]
+TRAIN_CATEGORIES: list[str] = ["Long-distance", "Commuter", "Cargo"]
+TRAIN_TYPES: list[str] = ["IC", "S", "PYO", "HDM", "HL", "T"]
 
-MAP_CENTER = [64.5, 26.0]
-MAP_ZOOM = 5
+MAP_CENTER: list[float] = [64.5, 26.0]
+MAP_ZOOM: int = 5
