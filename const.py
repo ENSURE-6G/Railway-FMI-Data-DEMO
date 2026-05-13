49e2baef716cc5d94dacc030466fe9ba3c0026aa
Vinicius Borin Zenbook <borin.vini@gmail.com>
2026-05-13 13:29:20 +0300

    feat: add MATCHED_DATA_PATH and MATCHED_FILE_PATTERN constants

--- const.py
+++ const.py
@@ -15,9 +15,14 @@ AVAILABLE_MONTHS: list[int] = list(range(1, 13))
 
 TRAIN_FILE_PATTERN = "all_trains_data_flat_{year}_{month:02d}.csv"
 WEATHER_FILE_PATTERN = "fmi_weather_observations_{year}_{month:02d}.csv"
+MATCHED_DATA_PATH = _DATA_ROOT / "matched_flat_data"
+MATCHED_FILE_PATTERN = "matched_data_flat_{year}_{month:02d}.csv"
 
 TRAIN_CATEGORIES: list[str] = ["Long-distance", "Commuter", "Cargo"]
 TRAIN_TYPES: list[str] = ["IC", "S", "PYO", "HDM", "HL", "T"]
 
 MAP_CENTER: list[float] = [64.5, 26.0]
 MAP_ZOOM: int = 5
+
+DEFAULT_ORIGIN = "Helsinki asema"
+DEFAULT_DESTINATION = "Rovaniemi"
